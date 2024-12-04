ff13fadef3bd0f2ab527866406848b3a8c0ea00b
Antony <antony.huart37@gmail.com>
2024-12-04 16:18:33 +0100

    TP-09

--- TP-09/09.sql
+++ TP-09/09.sql
@@ -19,7 +19,7 @@ CREATE TABLE projects (
 CREATE TABLE devis (
     ref_devis CHAR(20) NOT NULL,
     version_devis CHAR(5) NOT NULL,
-    montant VARCHAR(255) NOT NULL,
+    montant INT NOT NULL,
     project_id INT,
     ref_facture CHAR(20),
     CONSTRAINT pk_devis PRIMARY KEY (ref_devis)
@@ -28,7 +28,7 @@ CREATE TABLE devis (
 CREATE TABLE factures (
     ref_facture CHAR(20),
     info VARCHAR(255),
-    total VARCHAR(255),
+    total INT,
     creation_date DATE,
     paiment_date DATE,
     CONSTRAINT pk_factures PRIMARY KEY (ref_facture)
@@ -56,19 +56,52 @@ INSERT INTO projects (nom, client_id) VALUES
 
 
 INSERT INTO factures (ref_facture, info, total, creation_date, paiment_date) VALUES
-("FA001", "Site internet partie 1", "1 500 euros", "2023-09-01", "2023-10-01"),
-("FA002", "Site internet partie 2", "1 500 euros", "2023-09-20", NULL),
-("FA003", "Logiciel CRM", "5 000 euros", "2024-02-01", NULL),
-("FA004", "Logiciel devis", "3 000 euros", "2024-03-03", "2024-04-03"),
-("FA005", "Site internet ecommerce", "5 000 euros", "2023-03-01", NULL),
-("FA006", "logiciel ERP", "2 000 euros", "2023-03-01", NULL),
+("FA001", "Site internet partie 1", 1500, "2023-09-01", "2023-10-01"),
+("FA002", "Site internet partie 2", 1500, "2023-09-20", NULL),
+("FA003", "Logiciel CRM", 5000, "2024-02-01", NULL),
+("FA004", "Logiciel devis", 3000, "2024-03-03", "2024-04-03"),
+("FA005", "Site internet ecommerce", 5000, "2023-03-01", NULL),
+("FA006", "logiciel ERP", 2000, "2023-03-01", NULL);
 
 
 INSERT INTO devis (ref_devis, version_devis, montant, project_id, ref_facture) VALUES
-("DEV2100A", 1, "3 000 euros", 1, "FA001"),
-("DEV2100B", 2, "5 000 euros", 1, NULL),
-("DEV2100C", 1, "5 000 euros", 2, "FA003"),
-("DEV2100D", 1, "3 000 euros", 3, "FA004"),
-("DEV2100E", 1, "5 000 euros", 4, "FA005"),
-("DEV2100F", 1, "2 000 euros", 5, "FA006"),
-("DEV2100G", 1, "1 000 euros", 6, NULL);
+("DEV2100A", 1, 3000, 1, "FA001"),
+("DEV2100B", 2, 5000, 1, NULL),
+("DEV2100C", 1, 5000, 2, "FA003"),
+("DEV2100D", 1, 3000, 3, "FA004"),
+("DEV2100E", 1, 5000, 4, "FA005"),
+("DEV2100F", 1, 2000, 5, "FA006"),
+("DEV2100G", 1, 1000, 6, NULL);
+
+-- 1️⃣ Afficher toutes les factures avec le nom des clients
+SELECT f.ref_facture, c.nom, info, total, creation_date, paiment_date
+FROM factures f
+INNER JOIN devis d ON d.ref_facture = f.ref_facture
+INNER JOIN projects p ON p.id = d.project_id
+INNER JOIN clients c ON c.id = p.client_id
+ORDER BY f.ref_facture;
+
+-- 2️⃣ Afficher le nombre de factures par client
+-- Afficher 0 factures si il n'y a pas de factures
+SELECT c.nom, COUNT(f.ref_facture) AS nb_factures
+FROM clients c
+INNER JOIN projects p ON p.client_id = c.id
+INNER JOIN devis d ON d.project_id = p.id
+LEFT JOIN factures f ON f.ref_facture = d.ref_facture
+GROUP BY nom;
+
+-- 3️⃣ Afficher le chiffre d'affaire par client
+SELECT c.nom, SUM(montant)
+FROM clients c
+INNER JOIN projects p ON p.client_id = c.id
+INNER JOIN devis d ON d.project_id = p.id
+GROUP BY c.nom;
+
+-- 4️⃣ Afficher le CA total
+SELECT SUM(total) AS ca_total
+FROM factures;
+
+-- 5️⃣ Afficher la somme des factures en attente de paiement
+SELECT SUM(total) AS ca_total
+FROM factures;
+WHERE IFNULL(paiment_date); 
